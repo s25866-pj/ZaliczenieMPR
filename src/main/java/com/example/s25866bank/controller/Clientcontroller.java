5c9f76351436801325a8f479c9426804dce23fb9
s25866 <s25866@pjwstk.edu.pl>
2023-01-22 17:12:38 +0100

    Coś dodane v2

--- src/main/java/com/example/s25866bank/controller/Clientcontroller.java
+++ src/main/java/com/example/s25866bank/controller/Clientcontroller.java
@@ -1,6 +1,12 @@
 package com.example.s25866bank.controller;
 
+import com.example.s25866bank.Exception.DatabaseException;
+import com.example.s25866bank.model.Client;
 import com.example.s25866bank.service.ClientService;
+import jakarta.xml.bind.ValidationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,19 @@ public class Clientcontroller {
 
     public Clientcontroller(ClientService clientService) {
         this.clientService = clientService;
+    }
+    @PostMapping("/create")
+    public ResponseEntity<Client> createClient(@RequestBody Client client){
+//        try{
+            clientService.create(client);
+//        }
+//        catch (ValidationException validationException){
+//            return ResponseEntity.badRequest().build();
+//        }
+//        catch (DatabaseException databaseException){
+//            return ResponseEntity.unprocessableEntity().build();
+//        }
+        ResponseEntity.ok().build();
+        return null;
     }
 }
